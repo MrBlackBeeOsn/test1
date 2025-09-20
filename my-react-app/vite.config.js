a5c2913170787a2c30011593e2e023234b16e808
🐝 Mr. ßee - 🅾ng siêng năng📓 <122947792+MrBlackBeeOsn@users.noreply.github.com>
2025-09-20 07:29:49 +0000

    ye

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test1/', // ← thay bằng tên repo của bạn
+  base: '/my-react-app/', // ← thay bằng tên repo của bạn
   plugins: [react()],
 })
